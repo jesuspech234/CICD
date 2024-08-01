319507ba56147029cf3fb531d8b380101b4e1c51
Alex Pech <naruto-sasuke2546@outlook.com>
2024-07-31 22:21:08 -0600

    Cambios de archivos

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,3 +1,8 @@
-def test_integration():
-    response = client.get('/integration')
+import pytest
+import requests
+
+
+def test_intex():
+    response = requests.get('<http://localhost:5000/>')
     assert response.status_code == 200
+    assert response.json()['message'] == 'Hello, World!'
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,3 +1,15 @@
-def test_home_page():
-    response = client.get('/')
-    assert response.status_code == 200
+import pytest
+from app import create_app
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_index(client):
+    rv = client.get('/')
+    json_data = rv.get_json()
+    assert json_data['message'] == 'Hello, World!
